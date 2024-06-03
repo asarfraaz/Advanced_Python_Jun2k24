826ce13284a7ad849258bb39f95663079f467e8c
Sarfraaz Ahmed <sarfraaz@gmail.com>
2024-06-03 12:51:12 +0530

    read_prices to return a dictionary of latest prices

--- Food_Mart/report.py
+++ Food_Mart/report.py
@@ -3,17 +3,13 @@
 '''
 import csv
 
-def read_inventory(filename: str) -> list[tuple[str, int, float]]:
+def read_inventory(filename: str) -> list[dict[str, str|int|float]]:
     inv = list()
     with open(filename) as FH:
         data = csv.reader(FH)
         headers = next(data)
 
         for row in data:
-            #info = dict(name  = row[0],
-            #            quant = int(row[1]),
-            #            price = float(row[2]),
-            #           )
             info = {
                     'name' : row[0],
                     'quant': int(row[1]),
@@ -22,3 +18,15 @@ def read_inventory(filename: str) -> list[tuple[str, int, float]]:
             inv.append(info)
 
     return inv
+
+def read_prices(filename:str) -> dict[str, float]:
+    prices = dict()
+    with open(filename) as FH:
+        data = csv.reader(FH)
+        for row in data:
+            try:
+                prices[ row[0] ] = float(row[1])
+            except IndexError as e:
+                continue
+
+    return prices

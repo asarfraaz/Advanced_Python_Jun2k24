85ed4d22d7fc62ea6bd919bcf1a577c99049d05a
Sarfraaz Ahmed <sarfraaz@gmail.com>
2024-06-03 11:00:36 +0530

    using functions for integration

--- Food_Mart/invcost.py
+++ Food_Mart/invcost.py
@@ -3,17 +3,22 @@
 '''
 import csv
 
-total_cost = 0.0
+def inventory_cost(filename):
+    total_cost = 0.0
 
-with open("Data/inventory.csv") as FH:
-    data = csv.reader(FH)
-    # Skip the header line
-    headers = next(data)
+    with open(filename) as FH:
+        data = csv.reader(FH)
+        # Skip the header line
+        headers = next(data)
 
-    # Starts iterating from second line onwards
-    for row in data:
-        quant = int(row[1])
-        price = float(row[2])
-        total_cost += quant * price
+        # Starts iterating from second line onwards
+        for row in data:
+            quant = int(row[1])
+            price = float(row[2])
+            total_cost += quant * price
 
-print("Total cost:", total_cost)
+    return total_cost
+
+
+cost = inventory_cost('Data/inventory.csv')
+print("Total cost:", cost)

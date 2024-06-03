5e6bc6c30f13568a302a89c63fe7f12fbf7cffc5
Sarfraaz Ahmed <sarfraaz@gmail.com>
2024-06-03 10:29:29 +0530

    skip header line using next

--- Food_Mart/invcost.py
+++ Food_Mart/invcost.py
@@ -2,23 +2,17 @@
     Find the total cost of inventory
 '''
 
-# Initialise total = 0
 total_cost = 0.0
 
-# Open the file
 with open("Data/inventory.csv") as FH:
-    # Iterate through the lines
-    first_line = True
+    # Skip the header line
+    headers = next(FH)
+
+    # Starts iterating from second line onwards
     for line in FH:
-        if first_line:
-            first_line = False
-            continue
-        # Extract from each line, quant and price
         parts = line.split(',')
         quant = int(parts[1])
         price = float(parts[2])
-        # Add quant * price to total
         total_cost += quant * price
 
-# Print the total
 print("Total cost:", total_cost)

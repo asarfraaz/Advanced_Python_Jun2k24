c1ded44fb15428274694f64d57498af488b0d69f
Sarfraaz Ahmed <sarfraaz@gmail.com>
2024-06-03 23:28:40 +0530

    database starter code

---
+++ Sales/Data/db_prog.py
@@ -0,0 +1,27 @@
+'''
+    Program to fetch data from Database
+'''
+
+import sqlite3
+import os.path
+
+def display_rows(cur):
+    '''
+        display rows from a DB cursor
+    '''
+    for row in cur:
+        print(f'{row[1]:10} {row[-2]:3} {row[-1]}')
+
+db_file = 'students_info_db.sqlite'
+
+if not os.path.exists(db_file):
+    raise SystemExit(f'{db_file} not found')
+
+conn = sqlite3.connect(db_file)
+cur = conn.cursor()
+
+query = "SELECT * FROM 'Students'"
+
+cur.execute(query)
+display_rows(cur)
+conn.close()

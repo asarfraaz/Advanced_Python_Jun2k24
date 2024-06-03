c55d16e30089965bf8b087d283a45379d9f02a6a
Sarfraaz Ahmed <sarfraaz@gmail.com>
2024-06-03 10:50:18 +0530

    using csv module

--- Food_Mart/invcost.py
+++ Food_Mart/invcost.py
@@ -1,18 +1,19 @@
 '''
     Find the total cost of inventory
 '''
+import csv
 
 total_cost = 0.0
 
 with open("Data/inventory.csv") as FH:
+    data = csv.reader(FH)
     # Skip the header line
-    headers = next(FH)
+    headers = next(data)
 
     # Starts iterating from second line onwards
-    for line in FH:
-        parts = line.split(',')
-        quant = int(parts[1])
-        price = float(parts[2])
+    for row in data:
+        quant = int(row[1])
+        price = float(row[2])
         total_cost += quant * price
 
 print("Total cost:", total_cost)

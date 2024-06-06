d008fd11c07a719140ae24db64a736daae75e411
Sarfraaz Ahmed <sarfraaz@gmail.com>
2024-06-06 16:51:45 +0530

    created a new route for about us

--- Web/flask_project/src/mychoice.py
+++ Web/flask_project/src/mychoice.py
@@ -3,8 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home_view(): # https://127.0.0.1/
     return "<h1>My Choice Online Store</h1>"
 
+@app.route("/about")
+def about_view():
+    return "<h1>About Us</h1>"
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
